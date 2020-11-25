b0c3f177c0bdc528ed7b355284a30fb1994e0b48
keklingaiu <70243843+keklingaiu@users.noreply.github.com>
2020-11-24 19:41:01 -0500

    Basic Project Almost done

---
+++ NPC/NPC.gd
@@ -0,0 +1,29 @@
+extends KinematicBody
+
+onready var Dialogue = get_node("/root/Game/UI/Dialogue")
+
+var dialogue = [
+	"Welcome to Bullseye Island! (Press E to continue)"
+	,"You need to get your running shoes and shootin' glasses on for this challenge!"
+	,"Shoot and destroy all the targets along with the enemy drone before time is up!"
+	,"The Bullseye Island challenge starts as soon as you press E."
+]
+
+
+func _ready():
+	$AnimationPlayer.play("Idle")
+	Dialogue.connect("finished_dialogue", self, "finished")
+
+
+func _on_Area_body_entered(body):
+	Dialogue.start_dialogue(dialogue)
+
+
+func _on_Area_body_exited(body):
+	Dialogue.hide_dialogue()
+	
+func finished():
+	get_node("/root/Game/Target_Container").show()
+	Global.timer = 120
+	Global.update_time()
+	get_node("/root/Game/UI/Timer").start()
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# 05-3D-RPG
-Project 05 for C220
+# 3D-Character
+
+An animated 3D character that can be used in the 3D-RPG project.
+
+Based on the [Kenney Character Assets](https://kenney.itch.io/kenney-character-assets) provided by kenney.nl. The model and animations were compiled in Blender and then imported into Godot.
+
+## Implementation
+Created in Blender 2.90.1 and Godot 3.2.2
